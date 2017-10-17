45efaa4bc0550fa5d1c09ab3e3047522fbfa780d
Amanda Beiner <amandabeiner@Amandas-MacBook-Pro.local>
2017-10-16 20:17:49 -0400

    start react pdf

--- app/javascript/react/src/components/Cv.js
+++ app/javascript/react/src/components/Cv.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React from 'react'
 
+import { Document, Page } from 'react-pdf/build/entry.webpack'
+// import './resume.pdf'
+// import { Document } from 'react-pdf/build/entry.webpack'
+// import Resume from '../constants/resume.pdf'
+// <Document
+//   file={{url: "http://res.cloudinary.com/dpuzgzqir/raw/upload/v1508198427/resume_exbqkm.pdf"}}
+// />
 const Cv = props => {
   return (
     <div>
