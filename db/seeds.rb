a5505c239f80258a60101b656a0b847876a7aa00
Amanda Beiner <aebeiner@gmail.com>
2018-03-27 18:04:47 -0400

    Add sendfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Resume.create(url: "http://res.cloudinary.com/dpuzgzqir/image/upload/v1522188152/Anne_Gray_Fischer_CV_website_pkgcna.pdf")
