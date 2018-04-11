d71f04442c2c30ab5ef13f6942dd56dd1016c39f
Amanda Beiner <amanda@privy.com>
2018-04-11 17:51:37 -0400

    Security

--- db/schema.rb
+++ db/schema.rb
@@ -45,10 +45,6 @@ ActiveRecord::Schema.define(version: 20180411205153) do
     t.string "url", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "pdf_file_name"
-    t.string "pdf_content_type"
-    t.integer "pdf_file_size"
-    t.datetime "pdf_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
